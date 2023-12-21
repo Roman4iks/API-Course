d6446c439a203204d500baadb142de437967c68f
Maranius <romathebestprogramist@gmail.com>
2023-12-21 14:01:55 +0200

    InitProject

---
+++ main.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
