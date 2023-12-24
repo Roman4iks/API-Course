2d920d69947f22773aee17a6e14e6713af6a13de
Roman Ershov <48359688+Roman4iks@users.noreply.github.com>
2023-12-24 20:12:01 +0200

    Delete src/routes/secureRoutes.js

--- src/routes/secureRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const secureController = require('../controllers/secureController');
-const { authenticateToken } = require('../middleware/authMiddleware');
-
-const router = express.Router();
-
-router.get('/secure-data', authenticateToken, secureController.getSecureData);
-
-module.exports = router;
